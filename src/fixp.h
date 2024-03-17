21806dea4194a80743b7fa2ee5d8985d06af995e
maximspbu <st119024@student.spbu.ru>
2024-03-17 14:23:34 +0000

    fix fixed point arithmetic implementation, add nums addition

--- src/fixp.h
+++ src/fixp.h
@@ -4,26 +4,33 @@
 
 #include <vector>
 #include <cstdint>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
 
 
-template <class T>
-class FIXP{
+template <class T, class E>
+class FixedPointArithmetic{
     public:
-        FIXP(T num, uint8_t intPartSize, uint8_t fracPartSize);
-        void TransformToBinary(T num);
-        T TransformToDecimal();
-        T operator+(Fixp& num);
-        T operator-(Fixp& num);
-        T operator*(Fixp& num);
-        T operator/(Fixp& num);
-        bool operator<(Fixp& num);
-        bool operator==(Fixp& num);
-        bool operator>(Fixp& num);
+        FixedPointArithmetic(T num, E e):num_(static_cast<E>(num*pow(2, e))), e_(e){};
+        FixedPointArithmetic<T, E> operator+(FixedPointArithmetic<T, E>& fixp){
+            FixedPointArithmetic<T, E> fixp_(0, std::max(e_, fixp.e_));
+            fixp_.num_ = (num_  << (fixp.e_ - e_)) + fixp.num_;
+            return fixp_;
+        }
+        FixedPointArithmetic<T, E> operator-(FixedPointArithmetic<T, E>& num);
+        FixedPointArithmetic<T, E> operator*(FixedPointArithmetic<T, E>& num);
+        FixedPointArithmetic<T, E> operator/(FixedPointArithmetic<T, E>& num);
+        bool operator<(FixedPointArithmetic<T, E>& num);
+        bool operator==(FixedPointArithmetic<T, E>& num);
+        bool operator>(FixedPointArithmetic<T, E>& num);
+        friend std::ostream& operator<<(std::ostream& os, const FixedPointArithmetic<T, E>& fixp){
+            os << "num: " << fixp.num_/pow(2, fixp.e_) << ", e: " << fixp.e_ << '\n';
+            return os;
+        }
     private:
-        bool sign_;
-        std::vector<bool> intPart_;
-        std::vector<bool> fracPart_;
-        bool overflow_;
+        E num_;
+        E e_;
 };
 
 #endif //FIXP_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include "fixp.h"
 
 int main(){
+    double n1 = 1.23;
+    int e1 = 16;
+    double n2 = 2.4722;
+    int e2 = 16;
+    FixedPointArithmetic<double, int> num1(n1, e1);
+    FixedPointArithmetic<double, int> num2(n2, e2);
+    std::cout << num1;
+    std::cout << num2;
+    std::cout << num1 + num2;
     return 0;
 }
